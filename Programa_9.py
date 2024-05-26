de558065cb5e74961f634441a0bcd6dc410bf50b
IngRamirezJezhuah <150400667+IngRamirezJezhuah@users.noreply.github.com>
2024-05-25 22:41:35 -0600

    Programa 9- Contraseñas

--- Programa_9.py
+++ Programa_9.py
@@ -1,6 +1,5 @@
 #escribe un codigo que solicite al usuario que ingrese una contraseña hasta que ingrese la contraseña correcta (POO123)
 #programa 9- Contraseñas
-
 print("Inicio de sesion")
 contraseña = str(input("ingrese la contraseña:"))
 
