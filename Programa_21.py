d7b587310da830ba180d0b1475c9eb8e5fe81cf4
IngRamirezJezhuah <ramirezalvaradodavidjezhuah@gmail.com>
2024-06-03 20:31:15 -0600

    Programa 21-Tupla Desempaquetada

---
+++ Programa_21.py
@@ -0,0 +1,21 @@
+# crea un programa que contena una tupla con informacion de una persona
+#y desempaque la informacion en 5 diferentes variables para imprimir
+#por seoarado(por ejemplo nombre,edad.....) commiy "Programa 21-Tupla Desempaquetada"
+Tuplita = ("David", 19, "Ti_bis", "Mexicano", "Aqua")
+
+nombre, edad, carrera, nacionalidad, color_fav = Tuplita
+
+print("---------------------------")
+print("        ଘ(੭ˊᵕˋ)੭* ੈ✩‧₊")
+print("")
+print("ଘ(∩^o^)⊃━☆゜Nombre:", nombre)
+print("")
+print("(｡- .•)Edad:", edad)
+print("")
+print("(=^ ◡ ^=)Carrera:", carrera)
+print("")
+print("٩(ˊᗜˋ*)و Nacionalidad:", nacionalidad)
+print("")
+print("Color favorito:", color_fav)
+print("     ☆✧˖°⋆.˚☆✧˖°⋆.˚")
+print("---------------------------")
